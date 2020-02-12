b1f69d586bdfe0a4114ed84d14a7fb7c3540fb70
gustavoweb33 <lealgustavo33@gmail.com>
2020-02-12 16:12:22 -0600

    added inital files

---
+++ index.js
@@ -0,0 +1,4 @@
+var addNumn = function (num1, num2) {
+    return num1 + num2;
+};
+console.log(addNumn(1, 2));
--- index.ts
+++ index.ts
@@ -2,4 +2,4 @@ const addNumn = (num1: number, num2: number) => {
     return num1 + num2;
 }
 
-console.log(addNumn(1, 2))
+console.log(addNumn(1, 2));
