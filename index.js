4bd7421e814370a3e6974a4ee5ff546638d99f1e
gustavoweb33 <lealgustavo33@gmail.com>
2020-02-20 13:15:22 -0600

    moved files to src

--- index.js
+++
@@ -1,9 +0,0 @@
-var addAndHandle = function (n1, n2, callback) {
-    var result = n1 + n2;
-    var callbackResult = callback(result);
-    console.log(callbackResult);
-};
-addAndHandle(10, 20, function (result) {
-    var plusOne = result + 1;
-    return plusOne;
-});
